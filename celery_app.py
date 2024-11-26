9706a4cf54d677bcb264de87f6f64ae869e793af
Santiago <santiagopsa@gmail.com>
2024-11-26 17:38:14 -0500

    Fix Redis SSL configuration

--- celery_app.py
+++ celery_app.py
@@ -2,13 +2,21 @@ import os
 from celery import Celery
 
 # Configuración de Celery
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
 celery_app = Celery(
     'tasks',
-    broker=os.getenv("REDIS_URL", "redis://localhost:6379/0"),  # URL del broker (Redis en este caso)
-    backend=os.getenv("REDIS_URL", "redis://localhost:6379/0")  # Backend para almacenar resultados
+    broker=REDIS_URL,  # URL del broker (Redis en este caso)
+    backend=REDIS_URL  # Backend para almacenar resultados
 )
 
 celery_app.conf.update(
+    broker_use_ssl={
+        'ssl_cert_reqs': 'CERT_NONE'  # Puedes usar 'CERT_REQUIRED' en producción con certificados válidos
+    },
+    redis_backend_use_ssl={
+        'ssl_cert_reqs': 'CERT_NONE'  # Configuración para backend
+    },
     task_serializer='json',
     accept_content=['json'],
     result_serializer='json',
